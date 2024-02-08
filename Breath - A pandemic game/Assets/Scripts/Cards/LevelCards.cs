c80162272d699d308197bf81ca378085cdb70b7c
Rafael Okabe <69397032+RafaelOkabe246@users.noreply.github.com>
2024-02-08 15:21:14 -0300

    Delete Breath - A pandemic game directory

--- Breath - A pandemic game/Assets/Scripts/Cards/LevelCards.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Level cards")]
-public class LevelCards : ScriptableObject
-{
-    public List<Card> cards;
-}
